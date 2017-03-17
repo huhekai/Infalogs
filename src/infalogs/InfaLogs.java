041b5b4776f49d8cdff29b283ed9dceac1ee1fb7
Mikko Tuominen <mt2112@outlook.com>
2017-03-17 13:56:54 +0200

    XML-parser
    Changed parsing to xml file format.

--- src/infalogs/InfaLogs.java
+++ src/infalogs/InfaLogs.java
@@ -1,17 +1,12 @@
 package infalogs;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -39,8 +34,10 @@ public class InfaLogs implements Comparable<InfaLogs> {
     private void processLogfile() {
         try {
             readFile();
-            parseTimestamp();
-
+            if (stats.size() > 0) {
+                startTime = parseTimestamp(stats.get(0));
+                endTime = parseTimestamp(stats.get(stats.size()-1));
+            }
         } catch (IOException ex) {
             System.err.println(ex.getLocalizedMessage());
             success = false;
@@ -50,6 +47,11 @@ public class InfaLogs implements Comparable<InfaLogs> {
         }
     }
 
+    public Date parseTimestamp(LogEntry entry) throws ParseException {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss ", Locale.ENGLISH);
+        return df.parse(entry.date);
+    }
+    
     public boolean isSuccessful() {
         return success;
     }
@@ -78,69 +80,17 @@ public class InfaLogs implements Comparable<InfaLogs> {
     }
 
     private void readFile() throws FileNotFoundException, IOException {
-        InputStream fis;
-        BufferedReader br;
+        FileInputStream fis;
+        XMLLogParser parser = new XMLLogParser();
         fis = new FileInputStream(file);
-        br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
-
-        logs = parseLoadSummary(br);
-        br.close();
-
+       
+        logs = parser.parse(fis, filters);
+        
         stats = logFilter.filter(logs, filters);
         errors = logFilter.getErrors(logs);
+        fis.close();
         logs.clear();
         logs = null;
-    }
-
-    private List<LogEntry> parseLoadSummary(BufferedReader br) throws IOException {
-        boolean first = true;
-
-        List<LogEntry> list = new ArrayList<LogEntry>();
-        LogEntry le = new LogEntry();
-        String line;
-        
-        while ((line = br.readLine()) != null) {
-            if (line.length() > 14) {
-                String sevr = line.substring(10, 14);
-                sevr = sevr.trim();
-
-                if (!sevr.isEmpty()) {
-                    if (first) {
-                        first = false;
-                    } else {     
-                        list.add(le);
-                        le = new LogEntry();
-                    }
-                }
-                if (line.length() >= 10) {
-                    le.date += ((line.substring(0, 10) == null) ? "" : line.substring(0, 10).trim());
-                    le.date += " ";
-                }
-                if (line.length() >= 15) {
-                    le.severity += ((line.substring(10, 15) == null) ? "" : line.substring(10, 15).trim());
-                }
-                if (line.length() >= 20) {
-                    le.code += ((line.substring(15, 20) == null) ? "" : line.substring(15, 20).trim());
-                }
-                if (line.length() >= 30) {
-                    le.service += ((line.substring(20, 30) == null) ? "" : line.substring(20, 30).trim());
-                }
-                if (line.length() >= 30) {
-                    le.message += ((line.substring(30) == null) ? "" : line.substring(30).trim());
-                    le.message += " ";
-                }                             
-            }
-        }
-        return list;
-    }
-    private void parseTimestamp() throws ParseException {
-        if (stats.size() > 0) {
-            LogEntry le = stats.get(0);
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss ", Locale.ENGLISH);
-            startTime = df.parse(le.date);
-            le = stats.get(stats.size()-1);
-            endTime = df.parse(le.date);
-        }
     }
 
     @Override
--- src/infalogs/Main.java
+++ src/infalogs/Main.java
@@ -21,7 +21,7 @@ public class Main {
         FilenameFilter filter = new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
-                return name.endsWith(".txt");
+                return name.endsWith(".xml");
             }
         };
 
